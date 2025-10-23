de6ebda7935e48d3f322bb885c43f7d81b11a7da
Julián Arenas Guerrero <18464038+arenas-guerrero-julian@users.noreply.github.com>
2025-10-23 17:34:01 +0200

    Update test_serve_cli.py

--- tests/test_serve_cli.py
+++ tests/test_serve_cli.py
@@ -9,48 +9,6 @@ from pycottas_endpoint.__main__ import cli
 
 runner = CliRunner()
 
-out_formats = ["ttl", "nt", "xml", "jsonld", "trig"]
-
-
-def test_convert():
-    for out_format in out_formats:
-        with tempfile.NamedTemporaryFile(delete=True) as tmp_file:
-            out_file = str(f"{tmp_file}.{out_format}")
-            result = runner.invoke(
-                cli,
-                ["convert", "tests/resources/test2.ttl", "--output", out_file],
-            )
-            assert result.exit_code == 0
-            with open(out_file) as file:
-                content = file.read()
-                assert len(content) > 1
-
-    # Fix issue with python creating unnecessary temp files on disk
-    for f in glob.glob("<tempfile._TemporaryFileWrapper*"):
-        os.remove(f)
-
-
-def test_convert_oxigraph():
-    with tempfile.NamedTemporaryFile(delete=True) as tmp_file:
-        result = runner.invoke(
-            cli,
-            [
-                "convert",
-                "--store",
-                "oxigraph",
-                "tests/resources/test2.ttl",
-                "--output",
-                str(tmp_file),
-            ],
-        )
-        assert result.exit_code == 0
-        with open(str(tmp_file)) as file:
-            content = file.read()
-            assert len(content) > 1
-    # Fix issue with python creating unnecessary temp files on disk
-    for f in glob.glob("<tempfile._TemporaryFileWrapper*"):
-        os.remove(f)
-
 
 # NOTE: Needs to run last tests, for some reason patching uvicorn as a side effects on follow up tests
 
@@ -63,24 +21,6 @@ def test_serve(mock_run: MagicMock) -> None:
         cli,
         [
             "serve",
-            "tests/resources/test.nq",
-            "tests/resources/test2.ttl",
-            "tests/resources/another.jsonld",
-        ],
-    )
-    assert result.exit_code == 0
-
-
-@patch("pycottas_endpoint.__main__.uvicorn.run")
-def test_serve_oxigraph(mock_run: MagicMock) -> None:
-    """Test serve oxigraph, mock uvicorn.run to prevent API hanging"""
-    mock_run.return_value = None
-    result = runner.invoke(
-        cli,
-        [
-            "serve",
-            "--store",
-            "oxigraph",
             "tests/resources/test.nq",
             "tests/resources/test2.ttl",
             "tests/resources/another.jsonld",

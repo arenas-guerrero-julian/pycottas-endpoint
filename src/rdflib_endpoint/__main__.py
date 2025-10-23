e0c57a9ca2bc70469e0e25af65c50537031907a5
Jorge Riaño <jorgerianodelacruz@gmail.com>
2025-10-23 10:55:08 +0200

    Actualizo __main__.py para soportar archivos .cottas directamente

--- src/rdflib_endpoint/__main__.py
+++ src/rdflib_endpoint/__main__.py
@@ -15,7 +15,7 @@ def cli() -> None:
     """Quickly serve RDF files as SPARQL endpoint with RDFLib Endpoint"""
 
 
-@cli.command(help="Serve a local RDF files as a SPARQL endpoint by default on http://0.0.0.0:8000/sparql")
+@cli.command(help="Serve a local RDF file (or .cottas) as a SPARQL endpoint")
 @click.argument("files", nargs=-1)
 @click.option("--host", default="localhost", help="Host of the SPARQL endpoint")
 @click.option("--port", default=8000, help="Port of the SPARQL endpoint")
@@ -28,32 +28,37 @@ def serve(files: List[str], host: str, port: int, store: str, enable_update: boo
 def run_serve(files: List[str], host: str, port: int, store: str = "default", enable_update: bool = False) -> None:
     if store == "oxigraph":
         store = store.capitalize()
-    
+
+
     file = files[0]
 
+    # Si es un archivo .cottas
     if file.lower().endswith(".cottas"):
-        click.echo(click.style("INFO", fg="green") + f": 📦 Cargando archivo COTTAS → {files}")
+        click.echo(click.style("INFO", fg="green") + f": 📦 Cargando archivo COTTAS → {file}")
         g = Graph(store=pycottas.COTTASStore(file))
+
+    # Si es otro formato RDF
     else:
         g = Dataset(store=store, default_union=True)
         file_list = glob.glob(file)
         for f in file_list:
-                g.parse(f)
-                click.echo(
-                    click.style("INFO", fg="green")
-                    + ":     📥️ Loaded triples from "
-                    + click.style(str(files), bold=True)
-                    + ", for a total of "
-                    + click.style(str(len(g)), bold=True)
-                )
+            g.parse(f)
+            click.echo(
+                click.style("INFO", fg="green")
+                + ": 📥️ Loaded triples from "
+                + click.style(str(f), bold=True)
+                + ", for a total of "
+                + click.style(str(len(g)), bold=True)
+            )
 
+    # Crear y lanzar el endpoint SPARQL
     app = SparqlEndpoint(
         graph=g,
         enable_update=enable_update,
         example_query="""PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 SELECT * WHERE {
-    ?s ?o ?p .
+    ?s ?p ?o .
 } LIMIT 100""",
     )
     uvicorn.run(app, host=host, port=port)
@@ -61,7 +66,7 @@ SELECT * WHERE {
 
 @cli.command(help="Merge and convert local RDF files to another format easily")
 @click.argument("files", nargs=-1)
-@click.option("--output", default="localhost", help="Host of the SPARQL endpoint")
+@click.option("--output", default="output.ttl", help="Output file name")
 @click.option("--store", default="default", help="Store used by RDFLib: default or Oxigraph")
 def convert(files: List[str], output: str, store: str) -> None:
     run_convert(files, output, store)
@@ -73,12 +78,12 @@ def run_convert(files: List[str], output: str, store: str = "default") -> None:
     g = Dataset(store=store, default_union=True)
     for glob_file in files:
         file_list = glob.glob(glob_file)
-        for files in file_list:
-            g.parse(files)
+        for f in file_list:
+            g.parse(f)
             click.echo(
                 click.style("INFO", fg="green")
-                + ":     📥️ Loaded triples from "
-                + click.style(str(files), bold=True)
+                + ": 📥️ Loaded triples from "
+                + click.style(str(f), bold=True)
                 + ", for a total of "
                 + click.style(str(len(g)), bold=True)
             )
